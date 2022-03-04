935ed8f31c58e87529986d453bf6d0ae7f89a84b
Khaled Mashaly <kmmashaly@gmail.com>
2022-03-04 19:23:00 +0200

    Solve Leetcode #413

---
+++ leetcode/413-arithmetic-slices.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/arithmetic-slices/
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n < 3:
+            return 0
+
+        count = 2
+        diff = nums[1] - nums[0]
+        ans = 0
+
+        for i in range(2, n):
+            if nums[i] - nums[i - 1] == diff:
+                count += 1
+                if count > 2:
+                    ans += count - 2
+            else:
+                count = 2
+                diff = nums[i] - nums[i - 1]
+
+        return ans
